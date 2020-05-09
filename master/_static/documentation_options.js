35df8126a44a3fce17b05eb1fdf163926cca6285
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-05-09 23:53:53 +0000

    Update docs after building Travis build 2676 of pywr/pywr
    The docs were built from the branch 'master' against the commit
    034ef641a76e541f99a5ab82d721b50c413fb192.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pywr/pywr/jobs/685187136.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --deploy-repo pywr/pywr-docs --build-tags --built-docs pywr-docs/html master

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.1.dev8+gb2d7b26',
+    VERSION: '1.6.1.dev10+g034ef64',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
